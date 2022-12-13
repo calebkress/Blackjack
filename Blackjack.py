0820e197c5fef78940c969136bfc8f2ad76f7dc6
Caleb Kress <calebkress@gmail.com>
2022-12-13 15:07:08 -0600

    implement Python game logic

--- Blackjack.py
+++ Blackjack.py
@@ -125,4 +125,54 @@ def push(player, dealer):
     print('It\'s a push - try again.')
 
 
+# main game execution
+while True:
+    print('Welcome to Blackjack. Get as close to 21 as possible without going over. \n Dealer hits until he reaches 17. Aces count as 11 or 1.')
 
+    # create + shuffle Deck, deal cards to Player and Dealer
+    deck = Deck()
+    deck.shuffle()
+
+    player_hand = Hand()
+    player_hand.add_card(deck.deal())
+    player_hand.add_card(deck.deal())
+
+    dealer_hand = Hand()
+    dealer_hand.add_card(deck.deal())
+    dealer_hand.add_card(deck.deal())
+
+    # set up Player's chips
+    player_chips = Chips()
+
+    # ask Player for bet
+    take_bet(player_chips)
+
+    # show cards (w/ 1 Dealer card hidden)
+    show_some(player_hand, dealer_hand)
+
+    while isGameActive:
+        # ask player to Hit or Stand
+        hit_or_stand(deck, player_hand)
+        # show cards (w/ 1 Dealer card hidden)
+        show_some(player_hand, dealer_hand)
+        # run winning scenarios
+        if dealer_hand.value > 21:
+            dealer_busts(player_hand, dealer_hand, player_chips)
+        elif dealer_hand.value > player_hand.value:
+            dealer_wins(player_hand, dealer_hand, player_chips)
+        elif dealer_hand.value < player_hand.value:
+            player_wins(player_hand, dealer_hand, player_chips)
+        else:
+            push(player_hand, dealer_hand)
+    
+    # print Player's chip total
+    print('\nPlayer\'s winnings stand at ', player_chips.total)
+
+    # prompt for new game
+    new_game = input('Would you like to play again? Enter \'y\' or \'n\': ')
+    if new_game[0].lower() == 'y':
+        isGameActive = True
+        continue
+    else:
+        print('Thank you for playing.')
+        break
